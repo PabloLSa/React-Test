b56f8c10fbd74797efdfcc98a8109e1059b5ffa3
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-01-22 21:27:34 -0300

    testando o componente <NotFound.js />.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando Not Found', () => {
+  it('', () => {
+    renderWithRouter(<NotFound />);
+    // verifiando se há na tela o texto de página não encontrada, quando a página solicitada nãop existir
+    const pageResqueste = screen.getByText(/Page requested not found/i);
+    expect(pageResqueste).toBeInTheDocument();
+    // verifiando se há na tela o texto de página não encontrada, quando a página solicitada nãop existir
+    const imagePageNotFound = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(imagePageNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
